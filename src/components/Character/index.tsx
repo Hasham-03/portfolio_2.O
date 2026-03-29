dcbfa46c84060f94f62cfb25b4de233b50379bf0
Hasham-03 <hashim332004@gmail.com>
2026-03-29 14:46:06 +0530

    Fix avatar centering and remove delayed scene mount

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
 import Scene from "./Scene";
 
 const CharacterModel = () => {
-  const [showScene, setShowScene] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowScene(true), 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return showScene ? <Scene /> : null;
+  return <Scene />;
 };
 
 export default CharacterModel;
